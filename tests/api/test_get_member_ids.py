08d4b52b220fe774ffc8f6b1fea844db7f5e5894
nareephas <35759755+nareephas@users.noreply.github.com>
2018-02-15 15:47:57 +0700

    Update test_get_member_ids.py

--- tests/api/test_get_member_ids.py
+++ tests/api/test_get_member_ids.py
@@ -25,7 +25,7 @@ from linebot import (
 
 class TestLineBotApi(unittest.TestCase):
     def setUp(self):
-        self.tested = LineBotApi('channel_secret')
+        self.tested = LineBotApi('d42b9b5dc4df7ad44e9bddd5ee915fc7')
 
     @responses.activate
     def test_get_group_member_ids(self):
